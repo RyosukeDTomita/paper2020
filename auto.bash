c12fad9fdc31e8321e8618cccca8bfca3e53182e
RyosukeDTomita <d310151c2@gmail.com>
2020-02-19 22:44:21 +0900

    origin

---
+++ auto.bash
@@ -0,0 +1,39 @@
+
+#speedtest
+#starttime
+starttime=$(date)
+starttimeh=$(date +'%H')
+starttimem=$(date +'%M')
+starttimes=$(date +'%S')
+starttimej=$(date +'%j')
+#execute job
+#dclfrt copyr2.f && ./a.out && python notify.py && python frito.py 
+dclfrt ito.f && ./a.out && python notify.py && python frito.py 
+#
+#output how long does it take to execute jobs
+endtimeh=$(date +'%H')
+endtimem=$(date +'%M')
+endtimes=$(date +'%S')
+endtimej=$(date +'%j')
+
+j=$(($endtimej-$starttimej))
+h=$(($endtimeh-$starttimeh))
+m=$(($endtimem-$starttimem))
+s=$(($endtimes-$starttimes))
+if [ $((s)) -lt 0 ]; then
+    m=$(($m-1))
+    s=$(($s+60))
+fi
+if [ $((m)) -lt 0 ]; then
+    m=$(($m+60))
+    h=$(($h-1))
+fi
+if [ $((h)) -lt 0 ]; then
+    j=$(($j-1))
+    h=$(($h+24))
+fi
+echo "starttime=$starttime"
+echo "endtime=$(date)"
+echo "caltime=$j(days)$h(hours)$m(min)$s(s)"
+
+# source ./auto.bash
