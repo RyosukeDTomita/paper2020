94421987b022943d4c5dd579621a6f94a8461e01
tomita <d310151c2@gmail.com>
2020-03-05 18:22:32 +0900

    origin

--- fittingtest.bash
+++ fittingtest.bash
@@ -1,26 +1,31 @@
-#!bin/bash
+#!/bin/bash
 #parameter setting
 for AA in {1..12}; do
-    BB=`printf %02d ${AA}`
-    region=/0/115/0/75
-    proj=M15
-    frame=f10a10g10
-    psfile=/home3/tomita/tomitasoturon2020/JRA55/fit/$(BB)fit.ps
-    infile=/home3/tomita/tomitasoturon2020/JRA55/fit/$(BB)fit.dat
-    tmp=/home3/tomita/tomitasoturon2020/JRA55/fit/tmpfile
-    insamplefile=/home3/tomita/soturon2020/JRA55/fit/sample.dat
-    samplefile=/home3/tomita/tomitasoturon2020/JRA55/fit/outsample.dat
-    confile=/home3/tomita/tomitasoturon2020/JRA55/fit/$(BB)fit.png
-    #
-    #extract record number and free-air anomaly using awk↲
-    #
-    awk 'NR%25==0 {printf "%10.3f %10.3f %10.3f %10.3f\n",  $2,$1,$6,$5*0.00025' $infile > $tmp↲
-    awk '{print $1,$2,$3,$4}' $insamplefile > dat1↲
-    awk '{printf "%5.1f %5.1f %5.1f %5.1f\n", $1,$2,$3,$4*0.05 }' dat1 > $samplefile↲
+BB=`printf %02d ${AA}`
+region=/0/115/0/75
+proj=x0.2/0.2
+frame=a10f10/a10f10WeSn
+#fmemori a mozikannkaku g keiidosenkannkaku
+psfile=/home3/tomita/tomitasoturon2020/JRA55/fit/${BB}fit.ps
+infile=/home3/tomita/tomitasoturon2020/JRA55/fit/${BB}fit.dat
+tmp=/home3/tomita/tomitasoturon2020/JRA55/fit/tmpfile
+insamplefile=/home3/tomita/tomitasoturon2020/JRA55/fit/sampledata.dat
+samplefile=/home3/tomita/tomitasoturon2020/JRA55/fit/sample.dat
+confile=/home3/tomita/tomitasoturon2020/JRA55/fit/${BB}fit.png
+vector=0.15+e-W1p
+#
+#extract record number and free-air anomaly using awk↲
+#
+#    awk 'NR%25==0 {printf "%10.3f %10.3f %10.3f %10.3f\n",$2,$1,$6,$5*0.05' $infile > $tmp↲
+ awk 'NR%25==0 {print$2,$1,$6,$5*0.1}' $infile > $tmp
+ awk '{print$1,$2,$3,$4*0.1}' $insamplefile > $samplefile
+#    awk '{printf "%5.1f %5.1f %5.1f %5.1f\n", $1,$2,$3,$4*0.05 }' dat1 > $samplefile↲
 
-    gmt psxy $tmp -R$region -J$proj -Sv0.03/0.06/1.0 -Gblue -K -V > $psfile↲
-    gmt pscoast -R$region -J$proj -Dh -Ggray -Wthin,black -K -V -O >> $psfile↲
-    gmt psbasemap -R$region -J$proj -B$frame -K -V -O >> $psfile↲
-    gmt psxy $samplefile -R$region -J$proj -Sv0.03/0.06/1.0 -Gblue -O -V >> $psfile↲
-    convert -density 300x300 $psfile $confile↲
+gmt psxy $tmp -R$region -J$proj -Sv$vector -Gblack -K -V > $psfile
+#    gmt psxy $tmp -R$region -J$proj -Sv/0.1/0.1/0.1 -Gblack -K -V > $psfile↲
+#    gmt pscoast -R$region -J$proj -Dh -Ggray -Wthin,black -K -V -O >> $psfile↲
+gmt psbasemap -R$region -J$proj -B$frame -K -V -O >> $psfile
+gmt psxy $samplefile -R$region -J$proj -Sv$vector -Gblue -O -V >> $psfile↲
+convert -density 300x300 $psfile $confile
 done
+#
--- windfitting.f90
+++ windfitting.f90
@@ -3,7 +3,7 @@ program windfitting
     integer :: m,la,lo,a,la2,lo2,number0,b
     real :: pi,theta,zerolat,zerolong,zerolat75,zerolong75,d,d2,dd,dd2
     real :: gridlat,gridlong,c,c2,xd,yd
-    real,dimension(416017) :: lat,long,ew,ns,z,ang,zv
+    real,dimension(416017) :: lat,long,ew,ns,z,ang,zv,nsc,ewc,zc,angc
     real,dimension(416017) :: latc,longc
     real,dimension(75,115) :: sumew,sumns,sumang,sumz,aveew,avens,aveang,avez,sumzv
     character*120 namein,nameout,path,readfile,outfile,path2
@@ -13,21 +13,21 @@ program windfitting
     nameout = '00fit.dat'
 !-------set parameter--------
     pi = 4*atan(1.e0)
-    c = (41-34)/(141-131)
-    d = 34-131*c
+    c = (41.e0-34.e0)/(141.e0-131.e0)
+    d = 34.e0-131.e0*c
     c2 = c-1
-    d2 = 34-131*c2
-    theta = atan(c)
+    d2 = 34.e0-131.e0*c2
+    theta = -atan(c)
     zerolong=cos(theta)*131-sin(theta)*34!ito.f x,y=(0,0)
     zerolat=sin(theta)*131+cos(theta)*34
     zerolat75 = 40!ito.f x,y=(0,75)
     zerolong75 = (40-d2)/c2
     dd = zerolat75-zerolong75*c
-    dd2 = 41 - 141*c2
-    xd = (dd2 -d2)/115
-    yd = (dd -d)/75
-    gridlat = 6356.752/360
-    gridlong = gridlat*cos((40/180)*pi)
+    dd2 = 41.e0 - 141.e0*c2
+!    xd = (dd2 -d2)/115
+!    yd = (dd -d)/75
+!    gridlat = 6356.752/360
+!    gridlong = gridlat*cos((40/180)*pi)
 !*****************************************
 ! (zerolat75,zerolong75)
 !         ↓
@@ -66,6 +66,10 @@ do m = 1,12,1
         latc(a)=latc(a)-zerolat
         longc(a)=longc(a)*gridlong
         latc(a)=latc(a)*gridlat
+        nsc(a)=cos(theta)*ns(a)-sin(theta)*ew(a)!spin theta
+        ewc(a)=sin(theta)*ns(a)+cos(theta)*ew(a)
+        zc(a)=((nsc(a))**2+(ewc(a)**2))**0.5
+        angc(a)=atan2(nsc(a),ewc(a))
     end do
     do la = 1,75,1
         do lo = 1,115,1
@@ -75,10 +79,10 @@ do m = 1,12,1
             do a = 1,416017,1
                 zv(a)=((lat(a)-la*10)**2+(long(a)-lo*10)**2)**0.5!calculate distance
                 sumzv(la,lo)=sumzv(la,lo)+zv(a)
-                sumew(la,lo)=sumew(la,lo)+ew(a)*zv(a)
-                sumns(la,lo)=sumns(la,lo)+ns(a)*zv(a)
-                sumang(la,lo)=sumang(la,lo)+ang(a)*zv(a)
-                sumz(la,lo)=sumz(la,lo)+z(a)*zv(a)
+                sumew(la,lo)=sumew(la,lo)+ewc(a)*zv(a)
+                sumns(la,lo)=sumns(la,lo)+nsc(a)*zv(a)
+                sumang(la,lo)=sumang(la,lo)+angc(a)*zv(a)
+                sumz(la,lo)=sumz(la,lo)+zc(a)*zv(a)
                     !number0 = number0 + 1
             end do
         end do
@@ -88,14 +92,14 @@ do m = 1,12,1
             aveew(la,lo)=sumew(la,lo)/sumzv(la,lo)
             avens(la,lo)=sumns(la,lo)/sumzv(la,lo)
             avez(la,lo)=sumz(la,lo)/sumzv(la,lo)
-            aveang(la,lo)=sumang(la,lo)/sumzv(la,lo)
+            aveang(la,lo)=180*sumang(la,lo)/sumzv(la,lo)
         end do
     end do
     open(111,file=outfile)
     do la = 1,75,1
         do lo = 1,115,1
-            write(111,*)la,lo,aveew(la,lo),avens(la,lo),avez(la,lo),aveang(la,lo)
-!1001        format(6F9.3)
+            write(111,1001)la,lo,aveew(la,lo),avens(la,lo),avez(la,lo),aveang(la,lo)
+1001        format(I2,4x,I3,4F9.3)
         end do
     end do
     close(111)
