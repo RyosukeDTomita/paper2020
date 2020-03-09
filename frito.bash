2bf6b378425f001d30470806cb07f8ae53843708
tomita <d310151c2@gmail.com>
2020-03-09 17:34:43 +0900

    origin

--- frito.bash
+++ frito.bash
@@ -20,38 +20,46 @@ tmpfile1="${path}${directry}tmp1"
 tmpfile2="${path}${directry}tmp2"
 tmpfile3="${path}${directry}tmp3"
 tmpfile4="${path}${directry}tmp4"
-grdfile1="${tmpfile1}.grd"
 grdfile2="${tmpfile2}.grd"
 grdfile3="${tmpfile3}.grd"
+grdfile4="${tmpfile4}.grd"
 #
 #extract record number and free-air anomaly using awk
 #
-awk 'NR%30==0 {print $1,$2,$7,$8}' $infile >$tmpfile1
+awk 'NR%25==0 {print $1,$2,(180/3.14)*atan2($8,$7),sqrt($7^2+$8^2)*10}' $infile >$tmpfile1
 awk '{print $1,$2,$6}' $infile >$tmpfile2
 awk '{print $1,$2,$3}' $infile >$tmpfile3
 awk '{print $1,$2,$9}' $infile >$tmpfile4
 #
-makecpt -Crainbow -T0/24/2 -Z > rb.cpt
-xyz2grd $tmpfile2 -G$grdfile1 -I1 -R$region
-xyz2grd $tmpfile3 -G$grdfile2 -I1 -R$region
-xyz2grd $tmpfile4 -G$grdfile3 -I1 -R$region
+makecpt -Crainbow -T0/1200/800 -Z > rb.cpt
+xyz2grd $tmpfile2 -G$grdfile2 -I1 -R$region
 #
-gmt grdimage $grdfile1 -J$proj -Crb.cpt -B$frame  -K -V > $psfile1
+
+#vector and pressure
+gmt grdimage $grdfile2 -J$proj -Crb.cpt -B$frame  -K -V > $psfile1
 gmt psxy $tmpfile1 -R$region -J$proj -Sv$vector -Gblack -K -V -O >> $psfile1
 gmt psbasemap -R$region -J$proj -B$frame -O -V -K >> $psfile1
-gmt grdcontour $grdfile1 -C10 -A10 -R$region -J$proj -W1 -K -V -O >> $psfile1
-gmt psscale -D5/-1/10/0.5h -Crb.cpt -Bf1a2 -O -V >> $psfile1
+gmt grdcontour $grdfile2 -C50 -A100 -R$region -J$proj -W1 -K -V -O >> $psfile1
+gmt psscale -D5/-1/10/1.0h -Crb.cpt -Bf1a2 -O -V >> $psfile1
 convert -density 300x300 $psfile1 $confile1
 ##
-gmt grdimage $grdfile2 -J$proj -Crb.cpt -B$frame -K -V > $psfile2
+
+#sea surface temperature
+makecpt -Crainbow -T0/24/2 -Z > rb.cpt
+xyz2grd $tmpfile3 -G$grdfile3 -I1 -R$region
+gmt grdimage $grdfile3 -J$proj -Crb.cpt -B$frame -K -V > $psfile2
 gmt psbasemap -R$region -J$proj -B$frame -O -V -K >> $psfile2
-gmt grdcontour $grdfile2 -C10 -A10 -R$region -J$proj -W1 -K -V -O >> $psfile2
+gmt grdcontour $grdfile3 -C1 -A1 -R$region -J$proj -W1 -K -V -O >> $psfile2
 gmt psscale -D5/-1/10/0.5h -Crb.cpt -Bf1a2 -O -V >> $psfile2
 convert -density 300x300 $psfile2 $confile2
 ##
-gmt grdimage $grdfile3 -J$proj -Crb.cpt -B$frame -K -V > $psfile3
+
+#1layer depth
+makecpt -Crainbow -T0/300/100 -Z > rb.cpt
+xyz2grd $tmpfile4 -G$grdfile4 -I1 -R$region
+gmt grdimage $grdfile4 -J$proj -Crb.cpt -B$frame -K -V > $psfile3
 gmt psbasemap -R$region -J$proj -B$frame -O -V -K >> $psfile3
-gmt grdcontour $grdfile3 -C10 -A10 -R$region -J$proj -W1 -K -V -O >> $psfile3
+gmt grdcontour $grdfile4 -C10 -A10 -R$region -J$proj -W1 -K -V -O >> $psfile3
 gmt psscale -D5/-1/10/0.5h -Crb.cpt -Bf1a2 -O -V -K >> $psfile3
 convert -density 300x300 $psfile3 $confile3
 done
